98fc5feb08326d35d8b9c89a39c99efe0c1a0719
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-03-15 14:23:30 +0100

    add a human class

---
+++ src/main/java/hello/Human.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class Human extends Animal {
+    public String sayHello() {
+        return "Hi i'm a human";
+    }
+}
--- src/main/java/hello/Start.java
+++ src/main/java/hello/Start.java
@@ -3,7 +3,9 @@ package hello;
 public class Start {
     public static void main(String[] args) {
         Animal myDog = new Dog();
+        Animal mrFlip = new Human();
 
         System.out.println(myDog.sayHello());
+        System.out.println(mrFlip.sayHello());
     }
 }
